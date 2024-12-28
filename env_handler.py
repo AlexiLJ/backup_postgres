fb0d5f39545ed2cb14ca7214e0014a12524f6d36
AlexiLJ <leiko.oleksandr@gmail.com>
2024-12-28 13:27:26 +0100

    refactoring iteration 2 + logging

--- env_handler.py
+++ env_handler.py
@@ -1,10 +1,10 @@
 import json
 from pathlib import Path
 
-def get_git_parent_dir(path):
+def get_git_parent_dir(path: Path) -> Path:
     return Path(path) / '.git'
 
-def get_active_branch_name(path) -> str:
+def get_active_branch_name(path: Path) -> str:
     """
     :return: str
     """
@@ -16,18 +16,17 @@ def get_active_branch_name(path) -> str:
         if line[0:4] == "ref:":
             return line.partition("refs/heads/")[2]
 
-
-def var_getter(name: str, path):
+def var_getter(name: str, path: Path):
     '''
     Getting sys. variables based on the git branch
     :param name: str name of the sys. variable
-    :param storage: str|None
+    :param path: Path module path
     :return: Any
     '''
     branch = get_active_branch_name(path)
     pathes = Path(__file__).parent.resolve() / "pathes.json"
     with open(pathes, "r") as p:
-        storage = json.load(p).get(branch)  # environment variables
+        storage = json.load(p).get(branch)
     with open(Path(storage)) as st:
         var = json.load(st).get(name)
     return var
--- main.py
+++ main.py
@@ -1,97 +1,100 @@
 import re
 import subprocess
 import boto3
+import logging
+from pathlib import Path
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 from env_handler import var_getter
-from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO,  # Set to DEBUG for more detailed logs
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("upload_to_s3.log"),  # Log to a file
+        logging.StreamHandler()  # Log to the console
+    ]
+)
 
 parent_path = Path(__file__).resolve().parent
-print(parent_path)
 
-def run_bash_script(script_path, *args):
+def run_bash_script(script_path: Path, *args) -> str | None:
     """
     Runs a bash script with optional arguments and returns its output.
+
     :param script_path: Path to the bash script
     :param args: Arguments to pass to the script
-    :return: The standard output of the bash script
+    :return: str|None The standard output of the bash script
     """
     try:
         command = ["bash", script_path] + list(args)
         # Run the bash script and capture its output
         result = subprocess.run(command, check=True, text=True, capture_output=True)
-        # Return the script's standard output
         return result.stdout.strip()
 
     except subprocess.CalledProcessError as e:
-        print("Error executing script:", e)
-        print("Error Output:\n", e.stderr)
+        logging.error("Error executing script: %s", e)
+        logging.error("Error Output: %s", e.stderr)
         return None
 
 def upload_to_s3(file_path, bucket_name, directory, region="us-east-1"):
     """
     Uploads a file to a specific directory in an S3 bucket.
 
-    Args:
-        file_path (str): Path to the local file to upload.
-        bucket_name (str): Name of the S3 bucket.
-        directory (str): The directory (prefix) in the S3 bucket where the file should be uploaded.
-        region (str): AWS region of the S3 bucket (default is 'us-east-1').
-
-    Returns:
-        str: The S3 URL of the uploaded file if successful.
+    :param file_path: Path to the local file to upload
+    :param bucket_name: Name of the S3 bucket
+    :param directory: Directory (prefix) in the S3 bucket where the file should be uploaded
+    :param region: AWS region of the S3 bucket (default is 'us-east-1')
+    :return: str The S3 URL of the uploaded file if successful
     """
     try:
+        logging.info("Starting the upload process.")
         # Extract the file name from the local file path
         file_name = file_path.split("/")[-1]
-
+        logging.debug("File name extracted: %s", file_name)
         # Construct the full S3 key (directory + file name)
         s3_key = f"{directory}/{file_name}" if directory else file_name
 
-        # Create an S3 client
         s3_client = boto3.client('s3',
                                  aws_access_key_id=var_getter('AWS_ACCESS_KEY_ID', path=parent_path),
                                  aws_secret_access_key=var_getter('AWS_SECRET_ACCESS_KEY', path=parent_path),
                                  region_name=region)
 
-        # Upload the file
+        logging.info("Uploading to file '%s' to the '%s'.", file_name, s3_key)
         s3_client.upload_file(file_path, bucket_name, s3_key)
-
         # Generate the S3 URL
         s3_url = f"https://{bucket_name}.s3.{region}.amazonaws.com/{s3_key}"
-        print(f"File uploaded successfully to {s3_url}")
+        logging.info("File '%s' successfully uploaded to '%s'.", file_path, s3_url)
         return s3_url
 
     except FileNotFoundError:
-        print(f"Error: The file {file_path} was not found.")
+        logging.error("Error: The file '%s' was not found.", file_path)
     except NoCredentialsError:
-        print("Error: AWS credentials not found.")
+        logging.error("Error: AWS credentials not found.")
     except PartialCredentialsError:
         print("Error: Incomplete AWS credentials.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.error("An unexpected error occurred: %s", e)
+        raise
 
 if __name__ == "__main__":
-    # Replace with the actual path to your bash script
     script_path = parent_path / 'backup_postgres.sh'
-
-    # Example arguments to the script
     arguments = [var_getter('POSTGRESQL_NAME', path=parent_path),
                  var_getter('POSTGRESQL_USER', path=parent_path),
                  var_getter('POSTGRESQL_PASSWORD', path=parent_path),
                  str(parent_path)
                 ]
 
-    # Run the script and get the output
     output = run_bash_script(script_path, *arguments)
-    # Print the output
     if output:
-        print("Script Output:\n", output)
+        logging.info("Script Output: '%s'", output)
         pattern = r"(/home[^ ]*?/backup_postgres[^ ]*?\.dump)"
         matches = re.findall(pattern, output)
-        print("Script Output:\n", matches)
+        logging.info("PG dump file names: '%s'", matches)
 
         upload_to_s3(file_path=matches[0],
                      bucket_name=var_getter('AWS_STORAGE_BUCKET_NAME', path=parent_path),
                      directory='pg_backups',
                      region=var_getter('AWS_S3_REGION_NAME', path=parent_path)
                      )
+    else:
+        logging.error("No stdout returned.")
